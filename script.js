7c509ada5ab48815d274249b21b8c53685674c8c
Johann Felipe Gonzalez <lakfel@users.noreply.github.com>
2025-07-29 13:44:03 -0400

    Online storage

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let participantId = null;
+
 const canvas = document.getElementById("experimentCanvas");
 const ctx = canvas.getContext("2d");
 
@@ -6,13 +8,15 @@ const feedbacks = [
     {feedbackMode : "none",
       buffer: [0]
     }, 
-    {feedbackMode : "buffer",
-      buffer: [0, 10, 20]
+    {feedbackMode : "green",
+      buffer: [0, 10]
     }
 ];
-const amplitudes = [84, 168, 238, 336, 672]; // ejemplo en píxeles
-const widths = [10.5, 21, 42];       // diámetro del target
+//const amplitudes = [84, 168, 238, 336, 672]; // ejemplo en píxeles
+const amplitudes = [238, 336, 672]; // ejemplo en píxeles
+const widths = [21, 42, 84];       // diámetro del target
 const trialsPerCombination = 9;
+
 
 
 let trackingInterval = null;
@@ -24,11 +28,14 @@ let trialPositionBasedData = []; // array de trials con datos de posición
 let firstTrial = true; // para manejar el primer trial
 
 let currentTrialS = {
-  feedbackMode,
+  feedbackMode: null,
+  buffer: 0,
   cursorPositions: [],
   cursorPositionsInterval: [], // array de posiciones del cursor
   movementStartTime: null,
-  reachingTime: null,
+  reachingTimes: [],
+  outTimes: [],
+  inTarget: false,
   clickDownTime: null,
   clickUpTime: null,
   success: false,
@@ -44,15 +51,17 @@ let velocityChart2 = null;
 let movementStarted = false;
 let trackingStartTime = null;
 
+
+let feedbackConditions = []; // array de condiciones de feedback
 let blocks = [];      // Cada bloque es una combinación A-W con T trials
 let currentBlock = 0;
 let currentTrial = 0;
+let currentCondition = 0; // índice de la condición actual
 
 let randomStart = 0;
 let isExperimentStarted = false;
-let feedbackMode = "none";
+//let feedbackMode = "none";
 let targets = [];
-let results = [];
 let movementStartTime = null;
 let data = [];
 
@@ -63,7 +72,6 @@ const startButton = {
 };
 
 
-document.getElementById("feedbackMode").addEventListener("change", updateFeedbackMode);
 drawStartButton();
 
 // Maneja clics
@@ -77,28 +85,46 @@ canvas.addEventListener("click", (e) => {
   if (!isExperimentStarted) {
     if (isInsideCircle(clickX, clickY, startButton)) {
         startExperiment();
+        
     }
     return;
   }
 
-  handleClickOnTarget(clickX, clickY);
+  //handleClickOnTarget(clickX, clickY);
 });
 
+
+function generateConditions(orderIndex) {
+  feedbackConditions = [];
+  for (let feedback of feedbacks) {
+    for (let buf of feedback.buffer) {
+      feedbackConditions.push({ feedbackMode: feedback.feedbackMode, buffer: buf });
+    }
+  }
+  // Latin Square rotation (modulo N)
+  const N = feedbackConditions.length;
+  feedbackConditions = [...Array(N)].map((_, i) => feedbackConditions[(i + orderIndex) % N]);
+
+  return feedbackConditions;
+
+}
 
 function generateBlocks() {
   blocks = [];
 
-
-  for (let feedback of feedbacks) {
-      feedbackMode = feedback.feedbackMode;
-      for(let buf of feedback.buffer){
+  currentFeedback = feedbackConditions[currentCondition];
+  currentBuffer = currentFeedback.buffer;
+  currentFeedbackMode = currentFeedback.feedbackMode;
+  //for (let feedback of feedbacks) {
+  //    let feedbackMode = feedback.feedbackMode;
+  //    for(let buf of feedback.buffer){
           for (let A of amplitudes) {
             for (let W of widths) {
-              blocks.push({ A, W, feedbackMode, buffer: buf });
+              blocks.push({ A, W, feedbackMode: currentFeedbackMode, buffer: currentBuffer });
             }
           }
-        }
-    }
+      //  }
+    //}
   
   // Aleatorizar el orden de bloques
   blocks = shuffleArray(blocks);
@@ -145,16 +171,24 @@ function nextTrial() {
   if (currentTrial >= trialsPerCombination) {
     currentBlock++;
     currentTrial = 0;
+    firstTrial = true; 
     randomStart = Math.floor(Math.random() * 10); // reiniciar aleatorio entre 0 y 8
   }
-
+  stopCursorTracking();
   if (currentBlock >= blocks.length) {
-    endExperiment();
-    return;
+    currentCondition++;
+    if (currentCondition >= feedbackConditions.length) {
+      endExperiment();
+      return;
+    }
+    currentBlock = 0;
+    currentTrial = 0;
+    firstTrial = true; // Reiniciar el primer trial
+    generateBlocks();
   }
 
-  const { A, W } = blocks[currentBlock];
-  stopCursorTracking();
+  const { A, W, feedbackMode, buffer } = blocks[currentBlock];
+  
 
 
 
@@ -166,10 +200,11 @@ function nextTrial() {
   const now = performance.now();
 
   currentTrialS.movementStartTime = now;
+  currentTrialS.feedbackMode = feedbackMode;
+  currentTrialS.buffer = buffer;
   trackingStartTime = now;
   movementStarted = true;
-
-  startCursorTracking();
+  if(!firstTrial)  startCursorTracking();
 }
 
 function handleClickOnTarget(x, y) {
@@ -184,15 +219,6 @@ function handleClickOnTarget(x, y) {
   const distance = Math.sqrt(dx * dx + dy * dy);
 
   const success = distance <= activeTarget.radius;
-  results.push({
-    success,
-    target: (currentTrial + randomStart) * 5 % 9,
-    block: currentBlock,
-    trial: currentTrial,
-    A: blocks[currentBlock].A,
-    W: blocks[currentBlock].W,
-    time: performance.now(),
-  });
 
   //logEvent(success ? "hit" : "miss");
 
@@ -258,7 +284,7 @@ function startCursorTracking() {
       time
     };
 
-    currentTrialS.cursorPositionsInterval.push(pos);
+    //currentTrialS.cursorPositionsInterval.push(pos);
     //checkReaching(pos);
   }, 1); // cada 10 ms
 }
@@ -266,7 +292,7 @@ function startCursorTracking() {
 
 function checkReaching(pos) {
     const target = targets[(currentTrial + randomStart)* 5 % 9]; // Alterna entre los targets del par
-    const buffer = feedbackMode === "buffer" ? 10 : 0;
+    const buffer = currentTrialS.buffer || 0; // buffer de colisión
     const dx = pos.x - target.x;
     const dy = pos.y - target.y;
     const distance = Math.sqrt(dx * dx + dy * dy);
@@ -274,10 +300,18 @@ function checkReaching(pos) {
     
     if (distance < target.radius + buffer) {
         target.hit = true;
-        if(!currentTrialS.reachingTime) currentTrialS.reachingTime = pos.time;
+
+        if(!currentTrialS.inTarget){
+          currentTrialS.inTarget = true;
+          currentTrialS.reachingTimes.push(pos.time - currentTrialS.movementStartTime);
+        } 
     }
     else {
         target.hit = false;
+        if(currentTrialS.inTarget){
+          currentTrialS.inTarget = false;
+          currentTrialS.outTimes.push(pos.time - currentTrialS.movementStartTime);
+        }
     }
     draw();
 }
@@ -303,6 +337,52 @@ function drawStartButton() {
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   ctx.fillText("Start", startButton.x, startButton.y);
+
+ // Draw instructions
+  ctx.font = "18px Arial";
+  ctx.fillStyle = "black";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "top";
+  const instructions = [
+    "Welcome to the Fitts' Law Game!",
+    "Your task is to reach the targets as quickly and accurately as possible.",
+    "The first target of each set will be YELLOW.",
+    "Timing does NOT start until you reach the yellow target.",
+    "After clicking Start, the set will begin.",
+    "The target may or may not change color when reached,",
+    "as shown by the feedback indicator in the top-left corner."
+  ];
+  let y = startButton.y + startButton.radius + 30;
+  for (let line of instructions) {
+    ctx.fillText(line, canvas.width / 2, y);
+    y += 28;
+  }
+
+   // --- Indicador de feedback en la esquina superior izquierda ---
+    const indicatorX = 30;
+    const indicatorY = 30;
+    const circleRadius = 10;
+
+    // Dibuja círculo
+    ctx.beginPath();
+    ctx.arc(indicatorX, indicatorY, circleRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#007BFF";
+    ctx.fill();
+
+    
+    ctx.beginPath();
+    ctx.arc( indicatorX , 2 * indicatorY, circleRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#28a745";
+    ctx.fill();
+
+    // Dibuja texto
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "black";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "middle";
+    ctx.fillText(`Feedback: None `, indicatorX + 20,  indicatorY);
+    ctx.fillText(`Feedback: Green `, indicatorX + 20,  2 * indicatorY);
+
 }
 
 function isInsideCircle(x, y, circle) {
@@ -319,11 +399,40 @@ function draw() {
     ctx.beginPath();
     ctx.arc(t.x, t.y, t.radius, 0, Math.PI * 2);
     const isActive = (((currentTrial + randomStart)*5 % 9) === index);
-    const activeColor = (t.hit && feedbackMode != "none")? "#28a745" : "#007BFF"; // verde si hit, azul si no
-    //const isActive = index === currentPair[currentTargetIndex];
-    ctx.fillStyle = isActive ? activeColor: "gray";
+
+    const activeColor = (t.hit && feedbackConditions[currentCondition].feedbackMode != "none")? "#28a745" : "#007BFF"; // verde si hit, azul si no
+
+    let color = "gray";
+    if( isActive) {
+      color = activeColor;
+      if(firstTrial) {
+        color = "yellow"; // amarillo para el primer trial
+      } 
+    }
+    ctx.fillStyle = color;
     ctx.fill();
   });
+
+   // --- Indicador de feedback en la esquina superior izquierda ---
+  const indicatorX = 30;
+  const indicatorY = 30;
+  const circleRadius = 10;
+  const feedback = feedbackConditions[currentCondition].feedbackMode || "none";
+  const circleColor = (feedback === "none") ? "#007BFF" : "#28a745"; // azul o verde
+
+  // Dibuja círculo
+  ctx.beginPath();
+  ctx.arc(indicatorX, indicatorY, circleRadius, 0, Math.PI * 2);
+  ctx.fillStyle = circleColor;
+  ctx.fill();
+
+  // Dibuja texto
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "black";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "middle";
+  ctx.fillText(`Feedback: ${feedback}`, indicatorX + 20, indicatorY);
+  
 }
 
 function updateFeedbackMode() {
@@ -334,28 +443,39 @@ function updateFeedbackMode() {
 }
 
 
-function endExperiment() {
+async function endExperiment() {
   isExperimentStarted = false;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.font = "24px Arial";
   ctx.fillStyle = "black";
   ctx.textAlign = "center";
   ctx.fillText("¡Experimento finalizado!", canvas.width / 2, canvas.height / 2);
-  console.log("Resultados:", results);
+
+  try {
+    if (participantId) {
+      await db.collection("participants").doc(participantId).update({
+        completed: true,
+        endedAt: new Date().toISOString()
+      });
+    }
+  } catch (err) {
+    console.error("Error al actualizar participante:", err);
+  }
 }
 
-function startExperiment() {
+async function startExperiment() {
     randomStart = Math.floor(Math.random() * 10); // 0 a 9 aleatorio
     isExperimentStarted = true;
-    results = [];
     data = [];
     currentBlock = 0;
     currentTrial = 0;
+    await initializeParticipant();
+
     generateBlocks();
     const { A, W } = blocks[0];
     generateRingTargets(A, W);
     draw();
-    startCursorTracking();  
+   //startCursorTracking();  
 }
 
 canvas.addEventListener("mousedown", (e) => {
@@ -363,6 +483,8 @@ canvas.addEventListener("mousedown", (e) => {
   currentTrialS.clickDownTime = performance.now();
   trackingStartTime = performance.now();
 });
+
+
 canvas.addEventListener("mouseup", (e) => {
   if (!isExperimentStarted) return;
 
@@ -370,6 +492,12 @@ canvas.addEventListener("mouseup", (e) => {
 
   //const dx = e.offsetX - targets[currentPair[currentTargetIndex]].x;
   //const dy = e.offsetY - targets[currentPair[currentTargetIndex]].y;
+  if(firstTrial) {
+    firstTrial = false;
+    currentTrial++; // Avanzar al siguiente trial
+    draw(); // Redibujar para el siguiente trial
+    return; // No hacer nada en el primer click
+  }
   const dx = e.offsetX - targets[(currentTrial + randomStart) * 5 % 9].x;
   const dy = e.offsetY - targets[(currentTrial + randomStart) * 5 % 9].y;
   const distance = Math.sqrt(dx * dx + dy * dy);
@@ -383,22 +511,27 @@ canvas.addEventListener("mouseup", (e) => {
 
   currentTrialS.clickUpTime = currentTrialS.clickUpTime - currentTrialS.movementStartTime;
   currentTrialS.clickDownTime = currentTrialS.clickDownTime - currentTrialS.movementStartTime;
-  if (currentTrialS.reachingTime) {
+  /*if (currentTrialS.reachingTime) {
     currentTrialS.reachingTime = currentTrialS.reachingTime - currentTrialS.movementStartTime;
-  }
+  }*/
   currentTrialS.confirmationTime = currentTrialS.clickUpTime;
 
   trialData.push(currentTrialS);
   
   showTrialData(currentTrialS);
 
+  saveTrialToFirestore(currentTrialS);
+
   // Reiniciar estado para siguiente trial
   currentTrialS = {
-    feedbackMode,
+    feedbackMode: null,
+    buffer: 0,
     cursorPositions: [],
     cursorPositionsInterval: [],
     movementStartTime: null,
-    reachingTime: null,
+    reachingTimes: [],
+    outTimes: [],
+    inTarget: false,
     clickDownTime: null,
     clickUpTime: null,
     success: false,
@@ -424,13 +557,17 @@ canvas.addEventListener("mouseup", (e) => {
 function showTrialData(trial) {
   const infoEl = document.getElementById("trialInfo");
   
+  const lastReachingTime = trial.reachingTimes.length > 0 ? trial.reachingTimes[trial.reachingTimes.length - 1] : null;
+  const lastOutTime = trial.outTimes.length > 0 ? trial.outTimes[trial.outTimes.length - 1] : null;
+
   // Mostrar tiempos en números
   const lines = [
     `Feedback: ${currentTrialS.feedbackMode}`,
     `A: ${currentTrialS.A}`,
     `W: ${currentTrialS.W}`,
     `ID: ${currentTrialS.ID.toFixed(2)}`,
-    `Reaching Time: ${currentTrialS.reachingTime?.toFixed(2)} ms`,
+    `Reaching Time: ${lastReachingTime?.toFixed(2)} ms`,
+    `Out Time: ${lastOutTime?.toFixed(2)} ms`,
     `Confirmation Time: ${currentTrialS.confirmationTime?.toFixed(2)} ms`,
     `Click Duration: ${currentTrialS.clickDuration?.toFixed(2)} ms`,
     `Success: ${currentTrialS.success ? "✔️" : "❌"}`
@@ -442,10 +579,10 @@ function showTrialData(trial) {
   
 
   const { speeds, times } = getSpeedData(trial.cursorPositions);
-  velocityChart = drawSpeedChart("velocityChart", times, speeds, trial.reachingTime, trial.clickDownTime, trial.clickUpTime, velocityChart);
+  velocityChart = drawSpeedChart("velocityChart", times, speeds, trial.reachingTimes, trial.outTimes, trial.clickDownTime, trial.clickUpTime, velocityChart);
 
   const { speeds: speedsInterval, times: timesInterval } = getSpeedData(trial.cursorPositionsInterval);
-  velocityChart2 = drawSpeedChart("velocityChart2", timesInterval, speedsInterval, trial.reachingTime, trial.clickDownTime, trial.clickUpTime, velocityChart2);
+  velocityChart2 = drawSpeedChart("velocityChart2", timesInterval, speedsInterval, trial.reachingTimes, trial.outTimes, trial.clickDownTime, trial.clickUpTime, velocityChart2);
 }
 
 function getSpeedData(positions) {
@@ -478,7 +615,7 @@ function createVerticalLineAnnotation(label, color, value) {
   };
 }
 
-function drawSpeedChart(chartName, times, speeds, reachingTime, clickDownTime, clickUpTime, oldChart) {
+function drawSpeedChart(chartName, times, speeds, reachingTimes, outTimes, clickDownTime, clickUpTime, oldChart) {
 
   const ctx = document.getElementById(chartName).getContext("2d");
 
@@ -488,12 +625,24 @@ function drawSpeedChart(chartName, times, speeds, reachingTime, clickDownTime, c
 
   const annotations = {};
 
-  if (reachingTime) {
-    annotations.reach = createVerticalLineAnnotation(
-      "Reach",
-      "orange",
-      reachingTime 
-    );
+  if (reachingTimes.length > 0) {
+    for (let i = 0; i < reachingTimes.length; i++) {
+      annotations[`reach${i}`] = createVerticalLineAnnotation(
+        "Reach",
+        "orange",
+        reachingTimes[i]
+      );
+    }
+  }
+
+  if (outTimes.length > 0) {
+    for (let i = 0; i < outTimes.length; i++) {
+      annotations[`out${i}`] = createVerticalLineAnnotation(
+        "Out",
+        "red",
+        outTimes[i]
+      );
+    }
   }
   
   if (clickDownTime) {
@@ -507,7 +656,7 @@ function drawSpeedChart(chartName, times, speeds, reachingTime, clickDownTime, c
 if (clickUpTime) {
   annotations.clickUp = createVerticalLineAnnotation(
     "MouseUp",
-    "red",
+    "green",
     clickUpTime 
   );
 }
@@ -548,3 +697,48 @@ if (clickUpTime) {
   });
 }
 
+async function saveTrialToFirestore(trial) {
+  try {
+
+    const user = firebase.auth().currentUser;
+    if (!user) throw new Error("Usuario no autenticado");
+
+
+    await db.collection("fitts_trials").add({
+      participantId,
+      timestamp: new Date().toISOString(),
+      ...trial
+    });
+    console.log("Trial guardado en Firestore");
+  } catch (error) {
+    console.error("Error al guardar en Firestore:", error);
+  }
+}
+
+
+async function initializeParticipant() {
+  try {
+    const user = firebase.auth().currentUser;
+    if (!user) throw new Error("Usuario no autenticado");
+
+    participantId = user.uid;
+
+    // Genera un número aleatorio entre 0 y feedbackConditions.length - 1
+    let orderIndex = Math.floor(Math.random() * feedbackConditions.length);
+
+    feedbackConditions = generateConditions(orderIndex);
+    currentCondition  = 0; // Reiniciar al primer índice
+
+    await db.collection("participants").doc(participantId).set({
+      startedAt: new Date().toISOString(),
+      completed: false,
+      orderIndex: orderIndex,
+      feedbackConditions : feedbackConditions
+
+    });
+
+    console.log("Participante inicializado:", participantId, "orderIndex:", window.orderIndex);
+  } catch (err) {
+    console.error("Error creando participante:", err);
+  }
+}
